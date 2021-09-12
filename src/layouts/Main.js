4323e6b0033b34adf45b0dfdabc80edf56e7b176
Jim <jiminbentley@gmail.com>
2021-09-12 18:41:37 -0400

    descriptio change

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -39,7 +39,7 @@ Main.defaultProps = {
   children: null,
   fullPage: false,
   title: null,
-  description: "Michael D'Angelo's personal website.",
+  description: "BesT Deals",
 };
 
 export default Main;
