e7ca99ffbab9227d6146b33825aa68f8d3a4ea7c
Jim <jiminbentley@gmail.com>
2021-09-14 01:29:20 -0400

    9-14 changes

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -4,13 +4,13 @@ const Age = () => {
   const [age, setAge] = useState();
 
   const tick = () => {
-    const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const divisor = 60; // seconds in a hour
+    const birthTime = new Date('2021-09-14T03:00:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
   useEffect(() => {
-    const timer = setInterval(() => tick(), 25);
+    const timer = setInterval(() => tick(), 2500);
     return () => {
       clearInterval(timer);
     };
@@ -23,18 +23,6 @@ const data = [
     key: 'age',
     label: 'Current age',
     value: <Age />,
-  },
-  {
-    key: 'countries',
-    label: 'Countries visited',
-    value: 53,
-    link:
-      'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
-  },
-  {
-    key: 'location',
-    label: 'Current city',
-    value: 'New York, NY',
   },
 ];
 
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
 
@@ -13,10 +12,29 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">9/12/2021 Best Amazon Deals!</Link></h2>
-          <p>
-            The deals will expire soon!.
-          </p>
+          <div>
+            <h2>
+              we are opening soon!
+            </h2>
+            <div className="countdown">
+              <div className="container-day">
+                <h3 className="day">Time</h3>
+                <h3>Day</h3>
+              </div>
+              <div className="container-hour">
+                <h3 className="hour">Time</h3>
+                <h3>Hour</h3>
+              </div>
+              <div className="container-minute">
+                <h3 className="minute">Time</h3>
+                <h3>Minute</h3>
+              </div>
+              <div className="container-second">
+                <h3 className="second">Time</h3>
+                <h3>Second</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
       {data.map((project) => (
