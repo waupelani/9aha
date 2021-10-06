a332364d56b34f1568771f2ca5cbe1bd45b3145e
Jim <jiminbentley@gmail.com>
2021-10-06 11:43:30 -0400

    add t-shirt page

---
+++ src/data/shirts-data.js
@@ -0,0 +1,10 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'scooter',
+    link: 'https://amzn.to/2YrxSxW',
+    image: '/images/projects/strip.webp',
+  },
+];
+
+export default data;
--- src/pages/Shirts.js
+++ src/pages/Shirts.js
@@ -3,24 +3,13 @@ import React from 'react';
 import Main from '../layouts/Main';
 
 import Cell from '../components/Projects/Cell';
-import data from '../data/projects';
-import Personal from '../components/Stats/Personal';
+import data from '../data/shirts-data';
 
 const Index = () => (
   <Main
     description={"'s'"}
   >
     <article className="post" id="index">
-      <header>
-        <div className="title">
-          <div>
-            <h2>
-              Deal End in..
-            </h2>
-            <p><Personal /></p>
-          </div>
-        </div>
-      </header>
       {data.map((project) => (
         <Cell
           data={project}
